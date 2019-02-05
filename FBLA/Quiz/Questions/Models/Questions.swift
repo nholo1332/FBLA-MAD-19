b54976d8f085b29de5bc8009570d3be1e66ee924
Noah Holoubek <noahholo@icloud.com>
2019-02-05 14:45:56 -0600

    Added Competitive Events questions

--- FBLA/Quiz/Questions/Models/Questions.swift
+++ FBLA/Quiz/Questions/Models/Questions.swift
@@ -11,7 +11,51 @@ import UIKit
 struct questions {
     
     struct competitiveEvents {
-        
+        static let question1 = Question(
+            question: "How many different kinds of competitive events are there, when pertaining to the number of people involved.",
+            answers: [
+                Answer(answer: "3 (individual, team, chapter)", isRight: true),
+                Answer(answer: "1 (individual)", isRight: false),
+                Answer(answer: "4 (individual, quads, duos, chapter)", isRight: false),
+                Answer(answer: "3 (individual, quads, duos)", isRight: false)
+            ]
+        ),
+       static let question2 = Question(
+            question: "There are also certain events specifically for high school students of what grades?",
+            answers: [
+                Answer(answer: "9th", isRight: true),
+                Answer(answer: "10th", isRight: true),
+                Answer(answer: "11th", isRight: false),
+                Answer(answer: "12th", isRight: false)
+            ]
+        ),
+        static let question3 = Question(
+            question: "Individual and team events focus on what skills? (Best answer that fits all events)",
+            answers: [
+                Answer(answer: "Leadership and career development", isRight: true),
+                Answer(answer: "Creativity and marketing development", isRight: false),
+                Answer(answer: "Monetary development", isRight: false),
+                Answer(answer: "Collaboration development", isRight: false)
+            ]
+        ),
+        static let question4 = Question(
+            question: "Chapter events recognize what achievements?",
+            answers: [
+                Answer(answer: "Chapter management and growth", isRight: true),
+                Answer(answer: "Business education and growth", isRight: false),
+                Answer(answer: "Business management", isRight: false),
+                Answer(answer: "Career exploration and growth", isRight: false)
+            ]
+        ),
+        static let question5 = Question(
+            question: "How many total competitive events are there? ",
+            answers: [
+                Answer(answer: "72", isRight: true),
+                Answer(answer: "59", isRight: false),
+                Answer(answer: "78", isRight: false),
+                Answer(answer: "87", isRight: false)
+            ]
+        )
     }
     
     struct businessSkills {
